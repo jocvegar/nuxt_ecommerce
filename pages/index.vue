adc90058d6ed855f4be1951e41f9599292e90128
Jose Vega <jocvegar@gmail.com>
2021-07-12 09:19:00 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <v-card class="logo py-4 d-flex flex-column justify-center align-center">
+        <div class="my-10">
+          <NuxtLogo />
+          <VuetifyLogo />
+        </div>
+        <br />
+        <v-card-title class="headline my-10">
+          Welcome to the JocFy
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Meatball pizza roll personal, mushrooms ranch stuffed crust white
+            pizza sausage steak NY style lasagna. Ham onions black olives extra
+            cheese meatball. White pizza chicken wing ranch, mushrooms chicken
+            wing stuffed green bell peppers. Pesto lasagna pizza pan bbq rib.
+            Chicago style thin crust bbq rib pie pan ham onions pepperoni pork
+            meatball philly chicken ricotta chicken NY style. Meat lovers
+            chicken and bacon melted cheese chicken hand tossed onions ham mayo
+            bbq Chicago style pan extra cheese.
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>

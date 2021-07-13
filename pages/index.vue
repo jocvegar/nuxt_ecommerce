b397c5484799fab0d595cad578ff495da6d34fc8
Jose Vega <jocvegar@gmail.com>
2021-07-13 07:03:58 -0600

    adds product and category page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,73 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12">
-      <v-card class="logo py-4 d-flex flex-column justify-center align-center">
-        <div class="my-10">
+      <v-card class="logo py-4 d-flex flex-column align-center">
+        <div class="d-flex  justify-center my-10">
           <NuxtLogo />
           <VuetifyLogo />
         </div>
-        <br />
+
         <v-card-title class="headline my-10">
           Welcome to the JocFy
         </v-card-title>
         <v-card-text>
-          <p>
-            Meatball pizza roll personal, mushrooms ranch stuffed crust white
-            pizza sausage steak NY style lasagna. Ham onions black olives extra
-            cheese meatball. White pizza chicken wing ranch, mushrooms chicken
-            wing stuffed green bell peppers. Pesto lasagna pizza pan bbq rib.
-            Chicago style thin crust bbq rib pie pan ham onions pepperoni pork
-            meatball philly chicken ricotta chicken NY style. Meat lovers
-            chicken and bacon melted cheese chicken hand tossed onions ham mayo
-            bbq Chicago style pan extra cheese.
-          </p>
+          <div>
+            <h1>{{ merchant.business_name }}</h1>
+            <br />
+            <h3><n-link to="/categories">Categories</n-link></h3>
+            <category-list :categories="categories"></category-list>
+            <hr />
+            <h3><n-link to="/products">Products</n-link></h3>
+            <product-list :products="products"></product-list>
+          </div>
+        </v-card-text>
+        <v-card-text>
+          <h1>cliente side</h1>
+          <div v-if="loading === false">
+            <pre>{{ JSON.stringify(categorias, null, 2) }}</pre>
+          </div>
         </v-card-text>
       </v-card>
+      <hr class="my-10" />
+      <Tutorial />
     </v-col>
   </v-row>
 </template>
+
+<script>
+import commerce from "~/common/commerce";
+
+export default {
+  data() {
+    return {
+      loading: true,
+      categorias: [],
+      productos: []
+    };
+  },
+  mounted() {
+    this.fetcheData();
+    console.log(`commerce`, commerce);
+  },
+  methods: {
+    async fetcheData() {
+      const merchant = await commerce.merchants.about();
+      console.log(`merchant`, merchant);
+      const _categoias = await commerce.categories.list();
+      this.categorias = _categoias.data;
+      this.loading = false;
+    }
+  },
+  async asyncData({ $commerce }) {
+    const merchant = await $commerce.merchants.about();
+    const { data: categories } = await $commerce.categories.list();
+    const { data: products } = await $commerce.products.list();
+
+    return {
+      merchant,
+      categories,
+      products
+    };
+  }
+};
+</script>
